6cf3dbd70346677a775223e740d87b1fe76b3145
William de Bouter <97745377+Yowcry@users.noreply.github.com>
2024-09-15 18:48:06 +0200

    Update backend.php

--- backend/backend.php
+++ backend/backend.php
@@ -1,16 +1,13 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form inputs
     $name = $_POST['name'];
     $email = $_POST['email'];
 
-    // Set email parameters
     $to = "parahcorona@yahoo.com";
     $subject = "New Registration for Indonesia International Dance Festival";
     $message = "Name: " . $name . "\nEmail: " . $email;
     $headers = "From: no-reply@yourdomain.com\r\n";
     
-    // Send the email
     if (mail($to, $subject, $message, $headers)) {
         echo "Thank you for registering! We have received your information.";
     } else {
