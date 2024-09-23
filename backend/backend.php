7f8248ba7ffe6caf5d6fb2b151c8d9cc8524cbf1
William de Bouter <97745377+Yowcry@users.noreply.github.com>
2024-09-23 14:34:53 +0200

    updating noreply

--- backend/backend.php
+++ backend/backend.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = "indonesiadancefestival@gmail.com";
     $subject = "New Registration for Indonesia International Dance Festival";
     $message = "Name: " . $name . "\nEmail: " . $email;
-    $headers = "From: no-reply@yourdomain.com\r\n";
+    $headers = "From: no-reply@indonesiadancefestival.com\r\n";
     
     if (mail($to, $subject, $message, $headers)) {
         echo "Thank you for registering! We have received your information.";
